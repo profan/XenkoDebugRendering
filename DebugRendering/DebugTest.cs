33c01f076a35fa6ecbf5e632793b23e7283ed761
Robin Hübner <robinhubner@gmail.com>
2019-04-23 22:09:57 +0200

    ugh, raycasts look off

--- DebugRendering/DebugTest.cs
+++ DebugRendering/DebugTest.cs
@@ -322,8 +322,8 @@ namespace DebugRendering
                     var cameraWorldPos = CurrentCamera.Entity.Transform.WorldMatrix.TranslationVector;
                     var cameraUp = CurrentCamera.Entity.Transform.LocalMatrix.Up;
                     var cameraWorldNormal = Vector3.Normalize(result.Point - cameraWorldPos);
-                    DebugDraw.DrawLine(cameraWorldPos + cameraWorldNormal*-2.0f + (cameraUp * (-0.125f/4.0f)), result.Point, color: Color.HotPink, duration: 1.0f);
-                    DebugDraw.DrawArrow(result.Point, result.Point + result.Normal, coneHeight: 0.25f, coneRadius: 0.125f, color: Color.HotPink, duration: 1.0f);
+                    DebugDraw.DrawLine(cameraWorldPos + cameraWorldNormal*-2.0f + (cameraUp * (-0.125f/4.0f)), result.Point, color: Color.HotPink, duration: 5.0f);
+                    DebugDraw.DrawArrow(result.Point, result.Point + result.Normal, coneHeight: 0.25f, coneRadius: 0.125f, color: Color.HotPink, duration: 5.0f);
                     System.Diagnostics.Debug.WriteLine(result.Normal);
                 }
             }
